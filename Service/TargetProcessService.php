1264e597d4235e82e7bed5fa80d9b9696b3d5932
Wybren Koelmans <wybrenkoelmans@gmail.com>
2013-09-16 21:34:49 +0200

    initial commit

---
+++ Service/TargetProcessService.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace ZENben\TargetProcessBundle\Service;
+
+use ZENben\TargetProcessBundle\Buzz\Listener\TargetProcessListener;
+
+class TargetProcessService
+{
+    protected $browser;
+    protected $user;
+    protected $defaultProject;
+    
+    public function __construct(\Buzz\Browser $browser, $securityContext, $config) {
+        $browser->addListener(
+            new TargetProcessListener(
+                $config['api_base_url'], 
+                $config['username'], 
+                $config['password']
+            )
+        );
+        
+        $this->defaultProject = $config['default_project_id'];
+        $this->browser = $browser;
+        $this->user = $securityContext->getToken()->getUser();
+    }
+    
+    public function addRequest($title, $description, $project = null) {
+        $project = $project === null ? $this->defaultProject : $project;
+        $this->browser->post('/Requests',[],[
+            'Name' => $title,
+            'Description' => $description,
+            'Project' => ['Id' => $project]
+        ]);
+    }
+    
+}
---
+++ TargetProcessBundle.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace ZENben\TargetProcessBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class TargetProcessBundle extends Bundle
+{
+    
+}
